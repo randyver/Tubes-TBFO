c305a2af2dafb0bcc07de3cdf4d4838533529093
RealAzzmi <azmibazeid@gmail.com>
2023-11-26 15:50:37 +0700

    Add parser for attributes

--- html_parser.py
+++ html_parser.py
@@ -1,6 +1,6 @@
 import re
 
-def get_tokens(file_location: str) -> list[str]:
+def parse_no_attributes(file_location: str) -> list[str]:
     tokens = []
 
     with open(file_location) as file:
@@ -41,10 +41,10 @@ def get_tokens(file_location: str) -> list[str]:
                 tokens.append(''.join(clean_tag))
 
                 # Append all attributes of the tag 
-                pattern = r'(\w+)\s*=\s*["\'](.*?)["\']'
-                matches = re.findall(pattern, html_tag)
-                for attribute, _ in matches:
-                    tokens.append(attribute)
+                # pattern = r'(\w+)\s*=\s*["\'](.*?)["\']'
+                # matches = re.findall(pattern, html_tag)
+                # for attribute, _ in matches:
+                #     tokens.append(attribute)
 
             else:
                 tokens.append(''.join(tag))
@@ -53,6 +53,90 @@ def get_tokens(file_location: str) -> list[str]:
     return tokens
 
 
+def parse_attributes(file_location: str) -> list[str]:
+    tokens = []
 
-tokens = get_tokens('test/1.html')
+    with open(file_location) as file:
+        c = file.read(1)
+        while c != '':
+            # Ignore whitespaces.
+            if str.isspace(c):
+                c = file.read(1)
+                continue
+            
+            # If not a whitespace, but not part of a tag.
+            is_token_text = False
+            while c != '<':
+                is_token_text = True
+                c = file.read(1)
+
+            # if is_token_text:
+            #     tokens.append("TEXT")
+
+            # Start processing the tag.
+            tag = []
+            while c != '>':
+                tag.append(c)
+                c = file.read(1)
+            tag.append('>')
+
+            tag_string = ''.join(tag)
+
+            if tag[1] == '/':
+                c = file.read(1)
+                continue
+
+            if '="' in tag_string:
+                # clean_tag contains no attributes.
+                clean_tag = []
+                for character in tag:
+                    if character != ' ':
+                        clean_tag.append(character)
+                    else:
+                        break
+                clean_tag.append('>')
+                tokens.append(''.join(clean_tag))
+
+                attributes = dict(re.findall(r'(\S+)=["\']?((?:.(?!["\']?\s+(?:\S+)=|[>"\']))+.)["\']?', tag_string))
+                for attribute, value in attributes.items():
+                    tokens.append(attribute)
+                    if attribute == 'id':
+                        tokens.append('ID_VAL')
+
+                    elif attribute == "class":
+                        tokens.append('CLASS_VAL')
+                    
+                    elif attribute == 'style':
+                        tokens.append("STYLE_VAL")
+                    
+                    elif attribute == "alt":
+                        tokens.append("ALT_VAL")
+                    
+                    elif attribute == 'href':
+                        tokens.append("HREF_VAL")
+                    
+                    elif attribute == "action":
+                        tokens.append("ACTION_VAL")
+                    
+
+
+                    elif attribute == 'rel':
+                        tokens.append('REL_VAL')
+                    elif attribute == 'src':
+                        tokens.append("SRC_VAL")
+                    elif attribute == "type":
+                        tokens.append(value.replace(' ', ''))
+                    elif attribute == "method":
+                        tokens.append(value.replace(' ', ''))
+
+            else:
+                tokens.append(tag_string)
+
+            tokens.append("INIT")
+            c = file.read(1)
+    return tokens
+
+
+
+tokens = parse_attributes('test/1.html')
 print(tokens)
